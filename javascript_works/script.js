e452725e009abe7f94c872eabc3ca4626d4ac9ad
Athul Shaji <athulshaji34@gmail.com>
2021-09-09 14:25:26 +0530

    msg

---
+++ javascript_works/script.js
@@ -0,0 +1,6 @@
+// to print in console - console.log("")
+// Variable - to represent a memory location
+// var -globally access , can overwrite
+// const -cannot overwrite
+
+console.log("hello world");
